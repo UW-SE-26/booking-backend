f5aaf855b3a7720ed33baf60d49377bdff5d0edb
Sunny Zuo <me@sunnyzuo.com>
2021-08-05 21:09:15 -0600

    feat: initialize mongoose (mongoDB)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
 import { init as initDiscord } from './discord';
@@ -12,5 +13,19 @@ app.use('/api', apiRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Web app is listening on port ${process.env.PORT}`);
 });
+
+mongoose
+    .connect(`${process.env.MONGO_URI}`, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Successfully connected to MongoDB database');
+    })
+    .catch((e) => {
+        console.log(`Failed to connect to MongoDB database: ${e}`);
+    });
 
 initDiscord();
