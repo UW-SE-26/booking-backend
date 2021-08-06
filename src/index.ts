273f994ce2330173b0fb47d902a99c65acab2233
Alex Pawelko <hisaysajp@gmail.com>
2021-08-05 22:20:45 -0600

    Initialize express on mongoose callback

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,6 @@ import { init as initDiscord } from './discord';
 
 dotenv.config();
 
-const app = express();
-
-app.use('/api', apiRoutes);
-
-app.listen(process.env.PORT, () => {
-    console.log(`Web app is listening on port ${process.env.PORT}`);
-});
-
 mongoose
     .connect(`${process.env.MONGO_URI}`, {
         useNewUrlParser: true,
@@ -23,6 +15,13 @@ mongoose
     })
     .then(() => {
         console.log('Successfully connected to MongoDB database');
+    
+        const app = express();
+
+        app.use('/api', apiRoutes);
+        app.listen(process.env.PORT, () => {
+            console.log(`Web app is listening on port ${process.env.PORT}`);
+        });
     })
     .catch((e) => {
         console.log(`Failed to connect to MongoDB database: ${e}`);
