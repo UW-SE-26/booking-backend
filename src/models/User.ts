184a48d0fa0d9e19d881b4d3e74987c1d5041b7d
Sunny Zuo <me@sunnyzuo.com>
2021-08-07 11:39:57 -0600

    Format with Prettier

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,21 @@
 import { Schema, model } from 'mongoose';
 
 interface User {
-    name: string; 
-    email: string; 
-    password: string; 
-    registeredAt: Date; 
-    verified: boolean; 
+    name: string;
+    email: string;
+    password: string;
+    registeredAt: Date;
+    verified: boolean;
 }
 
-const userSchema = new Schema<User>({ 
-    name: String, 
-    email: { type: String, required: true }, 
+const userSchema = new Schema<User>({
+    name: String,
+    email: { type: String, required: true },
     password: String,
-    registeredAt: Date, 
-    verified: Boolean
+    registeredAt: Date,
+    verified: Boolean,
 });
 
-const userModel = model<User>('User', userSchema); 
+const userModel = model<User>('User', userSchema);
 
-export default userModel; 
+export default userModel;
