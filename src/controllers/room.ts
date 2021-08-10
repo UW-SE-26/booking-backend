d6b9768d9e94a5872bc33e749b0e7b2702ed867c
KevDev0247 <donkevin1980s@gmail.com>
2021-08-10 19:40:04 -0400

    Improving validation and code quality

--- src/controllers/room.ts
+++ src/controllers/room.ts
@@ -3,37 +3,43 @@ import Room from '../models/Room';
 
 export async function createRoom(req: Request, res: Response) {
     const { name, schedule, closed } = req.body;
-    if (typeof name === 'string' && typeof closed === 'boolean' && Array.isArray(schedule)) {
-        for (const day in schedule) {
-            if (typeof day !== 'string') {
-                console.log(typeof day);
-                return res.status(400).json({ err: 'validation failed' });
+    if (typeof name !== 'string' && typeof closed !== 'boolean' && !Array.isArray(schedule)) {
+        res.status(400).json({ err: 'validation failed' });
+        return;
+    }
+    for (const day of schedule) {
+        const expectedKeys = ['dateOfWeek', 'start', 'end'];
+        for (const key in day) {
+            if (!expectedKeys.includes(key)) {
+                res.status(400).json({ err: 'validation failed' });
+                return;
             }
         }
-        const room = new Room({
-            name,
-            schedule,
-            closed,
-        });
-        await room.save().catch((err) => {
-            return res.status(400).json({ err });
-        });
-        res.status(201).json({ room });
-    } else {
-        return res.status(400).json({ err: 'validation failed' });
     }
+    const room = new Room({
+        name,
+        schedule,
+        closed,
+    });
+    await room.save().catch((err) => {
+        res.status(400).json({ err });
+        return;
+    });
+    res.status(201).json({ room });
 }
 
 export async function queryRoom(req: Request, res: Response) {
     const id = req.query.id;
     if (id) {
         const room = await Room.findOne({ _id: id }).catch((err) => {
-            return res.status(404).json({ err });
+            res.status(404).json({ err });
+            return;
         });
         res.status(200).json({ room });
     } else {
         const rooms = await Room.find({}).catch((err) => {
-            return res.status(400).json({ err });
+            res.status(400).json({ err });
+            return;
         });
         res.status(200).json({ rooms });
     }
