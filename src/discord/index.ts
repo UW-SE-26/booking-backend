015b8985e360af69360649d423783e01bbddfdac
Ryan Chen <ryanchen54321@gmail.com>
2021-08-10 20:15:19 -0700

    feat: Added a way to deploy through the message command 'deploy' with prefix '$'

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -14,7 +14,7 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user!.tag}`);
 });
 
-//Temporary method to run commands until event handlers are created
+//Temporary method to run slash commands until event handlers are set up
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 
@@ -22,5 +22,23 @@ client.on('interactionCreate', async (interaction) => {
 
     if (command) {
         command.execute(interaction);
+    }
+});
+
+//Temporary method to initially set up the deploy slash command / run other message commands until event handlers are set up
+client.on('messageCreate', async (message) => {
+    if (!message.content.startsWith('$') || message.author.bot) return;
+
+    const messageContent = message.content.slice(1).replace(/\s+/g, ' ').trim().split(/ (.+)/);
+    const commandName = messageContent.shift()?.toLowerCase();
+
+    if (commandName === 'deploy') {
+        const data = {
+            name: 'deploy',
+            description: 'Deploys slash commands in current guild - meant for testing.',
+            options: [],
+        };
+
+        await message.guild!.commands.create(data);
     }
 });
