d8ce977abd411e9d27e24e4df4d174c13292878d
KevDev0247 <donkevin1980s@gmail.com>
2021-08-12 22:44:13 -0400

    Move room routes under api router

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
-import roomRoutes from './routes/api/room/room';
 import { init as initDiscord } from './discord';
 
 dotenv.config();
@@ -16,12 +15,10 @@ mongoose
     })
     .then(() => {
         console.log('Successfully connected to MongoDB database');
-
         const app = express();
 
         app.use(express.json());
         app.use('/api', apiRoutes);
-        app.use('/rooms', roomRoutes);
         app.listen(process.env.PORT, () => {
             console.log(`Web app is listening on port ${process.env.PORT}`);
         });
--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
+import roomRoutes from '../api/room/room';
 
 const router = Router();
+
+router.use('/rooms', roomRoutes);
 
 router.get('/', (req, res) => {
     res.send('Hello world!');
