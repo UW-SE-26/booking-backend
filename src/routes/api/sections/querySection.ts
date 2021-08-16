ddc8e0983e6751c62fb95b387f94af701030c95c
rosscleary <58639828+rosscleary@users.noreply.github.com>
2021-08-15 20:33:25 -0400

    fix: make requested changes to section query

--- src/routes/api/sections/querySection.ts
+++ src/routes/api/sections/querySection.ts
@@ -4,31 +4,33 @@ import timeblockModel from '../../../models/timeblock.model';
 
 const querySection = async (req: Request, res: Response): Promise<void> => {
     // Retrieves the room section with the id given in the link
-    const sectionInformation = await sectionModel.findOne({_id: req.params.id}).catch((error) => {
+    const sectionInformation = await sectionModel.findOne({ _id: req.params.id }).catch((error) => {
         res.status(404).json({ error });
         return;
     });
-    if(!sectionInformation) {
+    if (!sectionInformation) {
+        res.status(404);
         return;
     }
 
     // Retrieves all time blocks for the room section within the time range given in the input
     const sectionTimeblocks = await timeblockModel
-    .find({
-    sectionId: sectionInformation._id,
-    startsAt: {$gte: new Date(req.body.starts_at)},
-    endsAt: {$lte: new Date(req.body.ends_at)}
-    }).catch((error) => {
-        res.status(404).json({ error });
-        return;
-    });
+        .find({
+            sectionId: sectionInformation._id,
+            startsAt: { $gte: new Date(req.body.startsAt) },
+            endsAt: { $lte: new Date(req.body.endsAt) },
+        })
+        .catch((error) => {
+            res.status(404).json({ error });
+            return;
+        });
 
-    const section = ({
+    const section = {
         id: sectionInformation._id,
         name: sectionInformation.name,
         capacity: sectionInformation.capacity,
-        availableTimes: sectionTimeblocks
-    });
+        bookedTimes: sectionTimeblocks,
+    };
     res.status(201).json(section);
 };
 
