1fbd0217b742f1ad0c97ba0dc66fd427e6fefedd
KevDev0247 <donkevin1980s@gmail.com>
2021-08-17 16:23:34 -0400

    Temporary solution to querying sections

--- src/routes/api/rooms/queryRooms.ts
+++ src/routes/api/rooms/queryRooms.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Room from '../../../models/Room';
+import Section from '../../../models/section.model';
 
 const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
     const id = req.query.id;
@@ -8,12 +9,31 @@ const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
             res.status(404).json({ error });
             return;
         });
-        res.status(200).json({ room });
+        if (room) {
+            const sections = await Section.find({ roomId: room.id }).catch((error) => {
+                res.status(404).json({ error });
+                return;
+            });
+            if (sections) {
+                res.status(200).json({ room, sections });
+            } else {
+                res.status(200).json({ room });
+            }
+        }
     } else {
         const rooms = await Room.find({}).catch((error) => {
             res.status(400).json({ error });
             return;
         });
+
+        // if (rooms) {
+        //     for (const room of rooms) {
+        //         const sections = await Section.find({ roomId: room._id }).catch((error) => {
+        //             res.status(404).json({ error });
+        //             return;
+        //         });
+        //     }
+        // }
         res.status(200).json({ rooms });
     }
 };
