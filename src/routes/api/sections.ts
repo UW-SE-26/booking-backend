7b99077d0c3e99057f355c9321c74cd0b1d97b4b
KevDev0247 <donkevin1980s@gmail.com>
2021-08-21 15:04:46 -0400

    Auth integration in sections

--- src/routes/api/sections.ts
+++ src/routes/api/sections.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import querySectionRoute from './sections/querySection';
 import createSectionRoute from './sections/createSection';
+import authMiddleware from '../../middleware/auth/jwtVerify';
 
 const router = Router();
 
-router.get('/', querySectionRoute);
-router.post('/create', createSectionRoute);
+router.get('/', authMiddleware, querySectionRoute);
+router.post('/create', authMiddleware, createSectionRoute);
 
 export default router;
