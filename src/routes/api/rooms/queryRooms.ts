c74a884be8bfbf7816479311eef5314c26e50fd0
KevDev0247 <donkevin1980s@gmail.com>
2021-08-20 20:50:30 -0400

    Add comments to my code

--- src/routes/api/rooms/queryRooms.ts
+++ src/routes/api/rooms/queryRooms.ts
@@ -2,13 +2,21 @@ import { Request, Response } from 'express';
 import Room from '../../../models/Room';
 import Section from '../../../models/section.model';
 
+/**
+ * Route to query all sections or individual section
+ * @author Kevin Wang
+ */
 const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
     const id = req.query.id;
     if (id) {
+        // If an id is included in the query
+        // Retrieve the room and return the room
         const room = await Room.findOne({ _id: id }).catch((error) => {
             res.status(404).json({ error });
             return;
         });
+        // Temporary code to query sections
+        // Plan to use .populate()
         if (room) {
             const sections = await Section.find({ roomId: room.id }).catch((error) => {
                 res.status(404).json({ error });
@@ -21,6 +29,8 @@ const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
             }
         }
     } else {
+        // If no id is included in the query
+        // Retrieve all rooms
         const rooms = await Room.find({}).catch((error) => {
             res.status(400).json({ error });
             return;
--- src/routes/api/sections/createSection.ts
+++ src/routes/api/sections/createSection.ts
@@ -3,7 +3,12 @@ import { Types } from 'mongoose';
 import Section from '../../../models/section.model';
 import Room from '../../../models/Room';
 
+/**
+ * Route to create a section
+ * @author Kevin Wang
+ */
 const createSectionRoute = async (req: Request, res: Response): Promise<void> => {
+    // Get data from request and create section
     const { name, capacity, roomId } = req.body;
     const roomObjectId = Types.ObjectId(roomId);
     const section = new Section({
@@ -12,6 +17,7 @@ const createSectionRoute = async (req: Request, res: Response): Promise<void> =>
         roomObjectId,
     });
 
+    // Retrieve the room with the room id and push the section id into the list of section ids
     const room = await Room.findOne({ _id: roomObjectId }).catch((error) => {
         res.status(404).json({ error });
         return;
@@ -22,11 +28,12 @@ const createSectionRoute = async (req: Request, res: Response): Promise<void> =>
         res.status(404).json({ error: 'Room does not exist' });
         return;
     }
+
+    // Save room and sections
     await room.save().catch((error) => {
         res.status(400).json({ error });
         return;
     });
-
     await section.save().catch((error) => {
         res.status(400).json({ error });
         return;
