c7f52cb349e3ece30663c97f8f5a03c11326c1f5
Ryan Chen <ryanchen54321@gmail.com>
2021-08-26 23:13:04 -0400

    fix: removed unnecessary events

--- src/discord/events/index.ts
+++ src/discord/events/index.ts
@@ -1,7 +1,5 @@
 import ReadyEventHandler from './readyEventHandler';
 import CommandInteractionCreateEventHandler from './commandInteractionCreateEventHandler';
 import MessageCreateEventHandler from './messageCreateEventHandler';
-import SelectMenuInteractionCreateEventHandler from './selectMenuInteractionCreateEventHandler';
-import ButtonInteractionCreateEventHandler from './buttonInteractionCreateEventHandler';
 
-export default [ReadyEventHandler, CommandInteractionCreateEventHandler, SelectMenuInteractionCreateEventHandler, ButtonInteractionCreateEventHandler, MessageCreateEventHandler];
+export default [ReadyEventHandler, CommandInteractionCreateEventHandler, MessageCreateEventHandler];
