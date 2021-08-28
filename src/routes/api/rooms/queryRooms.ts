22f9a163d0d25d3c44de78d55aa818efb2f49af3
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-08-27 21:35:26 -0400

    feat: integrate sections and issues query on room api

--- src/routes/api/rooms/queryRooms.ts
+++ src/routes/api/rooms/queryRooms.ts
@@ -9,41 +9,26 @@ import Section from '../../../models/section.model';
 const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
     const id = req.query.id;
     if (id) {
-        // If an id is included in the query
-        // Retrieve the room and return the room
-        const room = await Room.findOne({ _id: id }).catch((error) => {
-            res.status(404).json({ error });
-            return;
-        });
-        // Temporary code to query sections
-        // Plan to use .populate()
-        if (room) {
-            const sections = await Section.find({ roomId: room.id }).catch((error) => {
+        // If an id is included in the query, retrieve the room and return the room and populate sections & issues field
+        const room = await Room
+            .findOne({ _id: id })
+            .populate('sections')
+            .populate('issues')
+            .catch((error) => {
                 res.status(404).json({ error });
                 return;
             });
-            if (sections) {
-                res.status(200).json({ room, sections });
-            } else {
-                res.status(200).json({ room });
-            }
-        }
+        res.status(200).json({ room });
     } else {
-        // If no id is included in the query
-        // Retrieve all rooms
-        const rooms = await Room.find({}).catch((error) => {
-            res.status(400).json({ error });
-            return;
-        });
-
-        // if (rooms) {
-        //     for (const room of rooms) {
-        //         const sections = await Section.find({ roomId: room._id }).catch((error) => {
-        //             res.status(404).json({ error });
-        //             return;
-        //         });
-        //     }
-        // }
+        // If no id is included in the query, retrieves all rooms and populate sections & issues field
+        const rooms = await Room
+            .find({})
+            .populate('sections')
+            .populate('issues')
+            .catch((error) => {
+                res.status(400).json({ error });
+                return;
+            });
         res.status(200).json({ rooms });
     }
 };
