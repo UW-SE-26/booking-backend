57ce0839fa64c60be7d9eb5d5548334941a0dbbd
Ross Cleary <rosscleary@rosswpcleary.com>
2021-09-02 20:39:28 -0400

    Booking create (#27)
    * fix: update time block model to contain bookings
    
    * feat: create booking endpoint, fix time block model for population, and fix discord errors that the changes caused
    
    * fix: update changes to section query and discord code
    
    * fix: formatting
    
    * fix: make changes to create booking endpoint

--- src/routes/api/sections.ts
+++ src/routes/api/sections.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import querySectionRoute from './sections/querySection';
 import createSectionRoute from './sections/createSection';
-import bookSectionRoute from './sections/bookSection';
 import authMiddleware from '../../middleware/jwtVerify';
 
 const router = Router();
 
 router.get('/', querySectionRoute);
 router.post('/create', authMiddleware, createSectionRoute);
-router.post('/book', authMiddleware, bookSectionRoute);
 
 export default router;
