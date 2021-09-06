0305b9138b857f5c12a042acaaebb0de8856200b
Ross Cleary <rosscleary@rosswpcleary.com>
2021-09-06 19:11:06 -0400

    feat: delete booking endpoint (#28)
    * fix: update time block model to contain bookings
    
    * feat: create booking endpoint, fix time block model for population, and fix discord errors that the changes caused
    
    * fix: update changes to section query and discord code
    
    * fix: formatting
    
    * fix: make changes to create booking endpoint
    
    * feat: create booking delete endpoint
    
    * fix: change id in delete booking request to path params and add user checking
    
    * fix: formatting

--- src/routes/api/bookings.ts
+++ src/routes/api/bookings.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import createBookingRoute from './bookings/createBooking';
+import deleteBookingRoute from './bookings/deleteBooking';
 import authMiddleware from '../../middleware/jwtVerify';
 
 const router = Router();
 
 router.post('/create', authMiddleware, createBookingRoute);
+router.delete('/delete/:id', authMiddleware, deleteBookingRoute);
 
 export default router;
