fda3ec5f4698a1989b12dfdc1c4a7e939a0cc28e
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 19:49:42 -0400

    fix: fix query by booker and remove auth requirement

--- src/routes/api/bookings.ts
+++ src/routes/api/bookings.ts
@@ -5,7 +5,7 @@ import authMiddleware from '../../middleware/jwtVerify';
 
 const router = Router();
 
-router.get('/', authMiddleware, queryBookingsRoute);
+router.get('/', queryBookingsRoute);
 router.post('/create', authMiddleware, createBookingRoute);
 
 export default router;
--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -3,15 +3,24 @@ import Booking from '../../../models/booking.model';
 
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
     const bookerDiscord = req.query.id;
-    if (bookerDiscord && typeof bookerDiscord === "string") {
-        const bookings = await Booking.find({ booker: bookerDiscord })
+    if (bookerDiscord) {
+        if (typeof bookerDiscord === "string") {
+            const bookings = await Booking.find({ booker: bookerDiscord })
+                .catch((error) => {
+                    res.status(404).json({ error });
+                    return;
+                });
+            res.status(200).json({ bookings });
+        } else {
+            res.status(404).json({ error: "Validation failed, booker is not valid" });
+        }
+    } else {
+        const bookings = await Booking.find({})
             .catch((error) => {
-                res.status(404).json({ error });
+                res.status(400).json({ error });
                 return;
             });
-        res.status(200).json({ bookings })
-    } else {
-        res.status(404).json({ error: "Validation failed, booker is not valid" });
+        res.status(200).json({ bookings });
         return;
     }
 }
