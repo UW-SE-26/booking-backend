79dad512e16c43901a4a0892e5ab3d51be34a0e6
Alex Pawelko <hisaysajp@gmail.com>
2021-09-06 14:18:12 -0400

    fix: Don't unnecessarily regenerate keypair and load it correctly (#29)

--- src/util/keypair.ts
+++ src/util/keypair.ts
@@ -1,15 +1,24 @@
 import fs from 'fs';
 import crypto from 'crypto';
 
-const keys = crypto.generateKeyPairSync('ed25519');
-
-const privateKey = keys.privateKey;
-const publicKey = keys.publicKey;
-
 const privateKeyFile = `${process.cwd()}/privkey.pem`;
-const publicKeyFile = `${process.cwd()}/pubkey.pem`;
 
-if (!fs.existsSync(privateKeyFile)) {
+let privateKey: crypto.KeyObject;
+let publicKey: crypto.KeyObject;
+
+if (fs.existsSync(privateKeyFile)) {
+    privateKey = crypto.createPrivateKey({
+        key: fs.readFileSync(privateKeyFile),
+        format: 'pem',
+    });
+    publicKey = crypto.createPublicKey({
+        key: fs.readFileSync(privateKeyFile), //generate public key from private key
+        format: 'pem',
+    });
+} else {
+    const keys = crypto.generateKeyPairSync('ed25519');
+    privateKey = keys.privateKey;
+    publicKey = keys.publicKey;
     fs.writeFileSync(
         privateKeyFile,
         privateKey.export({
@@ -17,15 +26,7 @@ if (!fs.existsSync(privateKeyFile)) {
             format: 'pem',
         })
     );
-}
-if (!fs.existsSync(publicKeyFile)) {
-    fs.writeFileSync(
-        publicKeyFile,
-        publicKey.export({
-            type: 'spki',
-            format: 'pem',
-        })
-    );
+    //not exporting the public key file since it's trivial to get it from the private key so let's save on some IO
 }
 
 export { privateKey, publicKey };
