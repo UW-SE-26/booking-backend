ce4b6e9094f7649d5e2560e5a0490dae4e43f771
rosscleary <58639828+rosscleary@users.noreply.github.com>
2021-09-07 01:04:16 -0400

    feat: make refactored booking delete and booking query endpoints

--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -1,18 +1,21 @@
 import { Request, Response } from 'express';
-import Booking from '../../../models/booking.model';
+import TimeBlockModel from '../../../models/timeBlock.model';
 
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
-    const { userEmail } = req.body;
-    let bookings;
-    if (userEmail) {
-        bookings = await Booking.find({ users: { $in: [userEmail] } });
-        res.status(200).json({ bookings });
-        return;
-    } else {
-        bookings = await Booking.find({});
-        res.status(200).json({ bookings });
-        return;
+    const userEmail = req.userEmail
+
+    const bookings = await TimeBlockModel.find({ users: { $in: [userEmail] } });
+    res.status(200).json({ bookings });
+
+    const returnBookings = [];
+    for (const booking of bookings) {
+        returnBookings.push({
+            isBooker: booking.booker === userEmail,
+            timeBlock: booking
+        });
     }
+
+    res.status(200).json(returnBookings);
 };
 
 export default queryBookingsRoute;
