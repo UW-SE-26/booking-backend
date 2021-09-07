046b61bf51a5bf9b1be65c37839e2a5c6bc62988
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 21:55:38 -0400

    fix: remove catches and convert endpont to post

--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -2,29 +2,20 @@ import { Request, Response } from 'express';
 import Booking from '../../../models/booking.model';
 
 /**
- * Route to query all bookings or the bookings from one booker
+ * Route to query all bookings or the bookings associated with a user
  * @author Kevin Wang
  */
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
-    const bookerEmail = req.query.id;
-    if (bookerEmail) {
-        if (typeof bookerEmail === 'string') {
-            // If an booker's discord id is included in the query, retrieve the bookings of the and return the booking
-            const bookings = await Booking.find({ booker: bookerEmail }).catch((error) => {
-                res.status(404).json({ error });
-                return;
-            });
-            res.status(200).json({ bookings });
-        } else {
-            // If an booker's discord id is not valid, return error
-            res.status(404).json({ error: 'Validation failed, booker is not valid' });
-        }
+    const { userEmail } = req.body;
+    let bookings;
+    if (userEmail) {
+        // If an user's email is included in the query, retrieve the bookings of the and return the bookings
+        bookings = await Booking.find({ users: { $in: [userEmail] }});
+        res.status(200).json({ bookings });
+        return;
     } else {
-        // If no booker's discord id is included in the query, retrieve all the bookings and return the booking
-        const bookings = await Booking.find({}).catch((error) => {
-            res.status(400).json({ error });
-            return;
-        });
+        // If no user's email is included in the query, retrieve all the bookings and return the bookings
+        bookings = await Booking.find({});
         res.status(200).json({ bookings });
         return;
     }
