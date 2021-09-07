d5bf80d1665f31b2ef9fb2a85e5aefec1a32338e
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 20:07:05 -0400

    fix: prettier code quality fix

--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -4,25 +4,23 @@ import Booking from '../../../models/booking.model';
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
     const bookerDiscord = req.query.id;
     if (bookerDiscord) {
-        if (typeof bookerDiscord === "string") {
-            const bookings = await Booking.find({ booker: bookerDiscord })
-                .catch((error) => {
-                    res.status(404).json({ error });
-                    return;
-                });
-            res.status(200).json({ bookings });
-        } else {
-            res.status(404).json({ error: "Validation failed, booker is not valid" });
-        }
-    } else {
-        const bookings = await Booking.find({})
-            .catch((error) => {
-                res.status(400).json({ error });
+        if (typeof bookerDiscord === 'string') {
+            const bookings = await Booking.find({ booker: bookerDiscord }).catch((error) => {
+                res.status(404).json({ error });
                 return;
             });
+            res.status(200).json({ bookings });
+        } else {
+            res.status(404).json({ error: 'Validation failed, booker is not valid' });
+        }
+    } else {
+        const bookings = await Booking.find({}).catch((error) => {
+            res.status(400).json({ error });
+            return;
+        });
         res.status(200).json({ bookings });
         return;
     }
-}
+};
 
 export default queryBookingsRoute;
