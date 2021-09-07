e8a1ba7c10d2b3f51c5ae3044605b658f87d10db
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 20:11:28 -0400

    fix: remove the routes under queryBookings

--- src/routes/api/sections.ts
+++ src/routes/api/sections.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 import querySectionRoute from './sections/querySection';
-import queryBookingsRoute from './bookings/queryBookings';
 import createSectionRoute from './sections/createSection';
 import authMiddleware from '../../middleware/jwtVerify';
 
 const router = Router();
 
 router.get('/', querySectionRoute);
-router.get('/bookings/active', queryBookingsRoute);
 router.post('/create', authMiddleware, createSectionRoute);
 
 export default router;
