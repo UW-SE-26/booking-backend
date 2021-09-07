3431f8bd930995cff0b0e495331906b6bb65bb9a
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 20:10:16 -0400

    fix: add comments

--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -1,19 +1,26 @@
 import { Request, Response } from 'express';
 import Booking from '../../../models/booking.model';
 
+/**
+ * Route to query all bookings or the bookings from one booker
+ * @author Kevin Wang
+ */
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
     const bookerDiscord = req.query.id;
     if (bookerDiscord) {
         if (typeof bookerDiscord === 'string') {
+            // If an booker's discord id is included in the query, retrieve the bookings of the and return the booking
             const bookings = await Booking.find({ booker: bookerDiscord }).catch((error) => {
                 res.status(404).json({ error });
                 return;
             });
             res.status(200).json({ bookings });
         } else {
+            // If an booker's discord id is not valid, return error
             res.status(404).json({ error: 'Validation failed, booker is not valid' });
         }
     } else {
+        // If no booker's discord id is included in the query, retrieve all the bookings and return the booking
         const bookings = await Booking.find({}).catch((error) => {
             res.status(400).json({ error });
             return;
