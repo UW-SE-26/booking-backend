4056a86f382a27dab0b3733808670d21f7c3ae45
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 22:25:00 -0400

    remove comments

--- src/routes/api/bookings/queryBookings.ts
+++ src/routes/api/bookings/queryBookings.ts
@@ -1,20 +1,14 @@
 import { Request, Response } from 'express';
 import Booking from '../../../models/booking.model';
 
-/**
- * Route to query all bookings or the bookings associated with a user
- * @author Kevin Wang
- */
 const queryBookingsRoute = async (req: Request, res: Response): Promise<void> => {
     const { userEmail } = req.body;
     let bookings;
     if (userEmail) {
-        // If an user's email is included in the query, retrieve the bookings of the and return the bookings
         bookings = await Booking.find({ users: { $in: [userEmail] } });
         res.status(200).json({ bookings });
         return;
     } else {
-        // If no user's email is included in the query, retrieve all the bookings and return the bookings
         bookings = await Booking.find({});
         res.status(200).json({ bookings });
         return;
