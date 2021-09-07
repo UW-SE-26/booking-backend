8afb9e894a0e747034bd21ead2c6b5286d5c45bc
Zhijun (Kevin) Wang <58012125+KevDev0247@users.noreply.github.com>
2021-09-06 22:40:11 -0400

    fix: refactor timeblock model and remove booking model

--- src/models/timeBlock.model.ts
+++ src/models/timeBlock.model.ts
@@ -1,19 +1,16 @@
 import { Schema, model, Types, Document, PopulatedDoc } from 'mongoose';
-import { Booking } from './booking.model';
 
 interface TimeBlock {
-    // Users that are signed up for time block
-    bookings: PopulatedDoc<Booking & Document>;
-    // Id of the room section that the time block corresponds to
+    users: [String];
+    booker: String;
     sectionId: Types.ObjectId;
     startsAt: Date;
     endsAt: Date;
 }
 
 const timeBlockSchema = new Schema<TimeBlock>({
-    // Users that are signed up for time block
-    bookings: [{ type: Types.ObjectId, ref: 'Booking' }],
-    // Id of the room section that the time block corresponds to
+    users: [{ type: String, required: true }],
+    booker: { type: String, required: true },
     sectionId: Types.ObjectId,
     startsAt: Date,
     endsAt: Date,
