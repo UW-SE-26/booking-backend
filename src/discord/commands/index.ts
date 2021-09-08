0f3b45514ee6b66b089299fa1d08971da46b9697
Ryan Chen <ryanchen54321@gmail.com>
2021-09-07 22:39:45 -0400

    feat: added a view command to view a user's booking

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -3,5 +3,6 @@ import Deploy from './deploy';
 import Rooms from './rooms';
 import Book from './book';
 import Manage from './manage';
+import View from './view';
 
-export default [Deploy, Rooms, Book, Manage, Ping];
+export default [Deploy, Rooms, Book, Manage, View, Ping];
