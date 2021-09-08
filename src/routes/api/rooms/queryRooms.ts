d26da1b5cb5d2fd2f1fc43cf4f1ae76fc026410f
Alex Pawelko <hisaysajp@gmail.com>
2021-09-07 21:33:54 -0400

    prettier format everything (shutup eslint)

--- src/routes/api/rooms/queryRooms.ts
+++ src/routes/api/rooms/queryRooms.ts
@@ -9,15 +9,11 @@ const queryRoomsRoute = async (req: Request, res: Response): Promise<void> => {
     const id = req.query.id;
     if (id) {
         // If an id is included in the query, retrieve the room and return the room and populate sections & issues field
-        const room = await Room.findOne({ _id: id })
-            .populate('sections')
-            .populate('issues');
+        const room = await Room.findOne({ _id: id }).populate('sections').populate('issues');
         res.status(200).json({ room });
     } else {
         // If no id is included in the query, retrieves all rooms and populate sections & issues field
-        const rooms = await Room.find({})
-            .populate('sections')
-            .populate('issues');
+        const rooms = await Room.find({}).populate('sections').populate('issues');
         res.status(200).json({ rooms });
     }
 };
