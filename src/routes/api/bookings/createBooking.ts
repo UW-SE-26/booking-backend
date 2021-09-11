bf3574bda52761b87b66d06c9983e05b7667ccab
Alex Pawelko <hisaysajp@gmail.com>
2021-09-11 15:35:54 -0400

    Fix date parsing

--- src/routes/api/bookings/createBooking.ts
+++ src/routes/api/bookings/createBooking.ts
@@ -3,12 +3,16 @@ import TimeBlock from '../../../models/timeBlock.model';
 import Section from '../../../models/section.model';
 import Room from '../../../models/room.model';
 import userModel from '../../../models/user.model';
+import { DateTime } from 'luxon';
 
 const createBookingRoute = async (req: Request, res: Response): Promise<void> => {
-    const { sectionId, userEmails, startAt } = req.body;
+    const { sectionId, userEmails, startsAt } = req.body;
+
+    const date = DateTime.fromISO(startsAt).toJSDate();
+
     const bookerEmail = req.userEmail;
     // Validating for time block nullability
-    const timeBlockRetrieved = await TimeBlock.findOne({ sectionId: sectionId, startAt: startAt });
+    const timeBlockRetrieved = await TimeBlock.findOne({ sectionId: sectionId, startsAt: date });
     if (timeBlockRetrieved) {
         res.status(400).json({ error: 'booking unsuccessful, timeBlock already existed' });
         return;
@@ -37,7 +41,7 @@ const createBookingRoute = async (req: Request, res: Response): Promise<void> =>
         sectionId,
         userEmails,
         bookerEmail,
-        startAt,
+        date,
     });
     res.status(200).json({ timeBlock: newTimeBlock });
 };
