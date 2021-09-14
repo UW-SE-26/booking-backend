17d9bacd7feaabd1fea83a26015e43489b4bdffb
Sunny Zuo <me@sunnyzuo.com>
2021-09-13 23:52:49 -0400

    fix: disable report command

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -3,7 +3,7 @@ import Rooms from './rooms';
 import Book from './book';
 import Manage from './manage';
 import View from './view';
-import Report from './report';
+// import Report from './report'; - temporarily disabled due to bug
 import Delete from './delete';
 
-export default [Rooms, Book, Manage, View, Delete, Report, Ping];
+export default [Rooms, Book, Manage, View, Delete, Ping];
