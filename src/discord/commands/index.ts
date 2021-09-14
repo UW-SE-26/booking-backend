5400281a51670aec2c923e9ca3fc1eb707a0607a
Sunny Zuo <me@sunnyzuo.com>
2021-09-13 23:51:22 -0400

    refactor: deploy command

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,5 +1,4 @@
 import Ping from './ping';
-import Deploy from './deploy';
 import Rooms from './rooms';
 import Book from './book';
 import Manage from './manage';
@@ -7,4 +6,4 @@ import View from './view';
 import Report from './report';
 import Delete from './delete';
 
-export default [Deploy, Rooms, Book, Manage, View, Delete, Report, Ping];
+export default [Rooms, Book, Manage, View, Delete, Report, Ping];
