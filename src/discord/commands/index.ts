bdc7a7bd8b436adf00081bdf981859246751c1cb
Sunny Zuo <me@sunnyzuo.com>
2021-09-14 14:06:28 -0400

    refactor: rename rooms command

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,9 +1,9 @@
 import Ping from './ping';
-import Rooms from './rooms';
 import Book from './book';
+import ManualBook from './manualbook';
 import Manage from './manage';
 import View from './view';
 // import Report from './report'; - temporarily disabled due to bug
 import Delete from './delete';
 
-export default [Rooms, Book, Manage, View, Delete, Ping];
+export default [Book, ManualBook, Manage, View, Delete, Ping];
